1cb726349c9d6bd4269fd250804d6a6533d0c31b
Nikitha Pulluri <npulluri26@gmail.com>
2025-07-10 18:40:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,10 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-#import pandas
+import pandas as pd
 
-# Write directly to the app
 st.title(f"🥤Customize Your Smoothie!🥤")
-st.write(
-  """Replace this example with your own code!
-  **And if you're new to Streamlit,** check
-  out our easy-to-follow guides at
-  [docs.streamlit.io](https://docs.streamlit.io).
-  """
-)
-
+st.write()
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The  name on your smoothie will be:", name_on_order)
@@ -22,13 +14,13 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'), col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
-#convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
-#pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
+# convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:'
@@ -43,8 +35,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        #search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ',fruit_chosen,' is ', search_on, '.')
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ',fruit_chosen,' is ', search_on, '.')
   
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
